e8861600e75b37c0f69e3343321004f9fd2961c2
Dmitry <scannorone@gmail.com>
2017-01-26 14:46:09 +0700

    Структура узлов

--- main.c
+++ main.c
@@ -1,4 +1,19 @@
+#include <malloc.h>
 #include <stdio.h>
+
+struct Node {
+    int key;
+    int height;
+    struct Node* left;
+    struct Node* right;
+};
+
+struct Node* makeNode(int key) {
+    struct Node* node = (struct Node*) malloc(sizeof(struct Node));
+    node->key = key;
+    node->height = 1;
+    node->left = node->right = NULL;
+}
 
 int main(int argc, char *argv[])
 {
